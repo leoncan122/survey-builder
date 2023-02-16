7c77e7a0f24f90f856087c8c225a4e526640381c
leoncan122 <leon.can122@gmail.com>
2023-02-16 13:58:09 +0100

    delete npmrc

--- netlify.preinstall.js
+++ netlify.preinstall.js
@@ -13,33 +13,36 @@ const authToken = process.env.NPM_TOKEN;
 //   `//npm.pkg.github.com/:_authToken=${process.env.NPM_TOKEN}\n//npm.pkg.github.com/:_header:Authorization=token ${process.env.NPM_TOKEN}\n@${scope}:registry=https://npm.pkg.github.com\n`,
 //  );
 // Only run this script on Netlify, this is a default Netlify environment variable
-// if (process.env.NETLIFY === 'true') {
-//   // Check if this has already run (we spawn yarn again at the end)
-//   if (process.env.NETLIFY_NPMRC_DONE === 'true') {
-//     return;
-//   }
+console.log("script fuera if", authToken)
 
-//   // Check if .npmrc already exists, if it does then do nothing (otherwise we create an infinite yarn loop)
-//   if (fs.existsSync('.npmrc')) {
-//     console.warn('Skipping .npmrc write because it already exists');
-//     return;
-//   }
+if (process.env.NETLIFY === 'true') {
+  console.log("script pasa if", authToken)
+  // Check if this has already run (we spawn yarn again at the end)
+  if (process.env.NETLIFY_NPMRC_DONE === 'true') {
+    return;
+  }
 
-//   console.log(`Creating .npmrc with auth tokens for @${scope}, provided by GITHUB_TOKEN environment variable...`);
+  // Check if .npmrc already exists, if it does then do nothing (otherwise we create an infinite yarn loop)
+  if (fs.existsSync('.npmrc')) {
+    console.warn('Skipping .npmrc write because it already exists');
+    return;
+  }
 
-//   // Create .npmrc
-//   fs.writeFileSync(
-//     '.npmrc',
-//     `//npm.pkg.github.com/:_authToken=${authToken}\n//npm.pkg.github.com/:_header:Authorization=token ${authToken}\n@${scope}:registry=https://npm.pkg.github.com\n`,
-//   );
+  console.log(`Creating .npmrc with auth tokens for @${scope}, provided by GITHUB_TOKEN environment variable...`);
 
-//   // Is this necessary?
-//   fs.chmodSync('.npmrc', 0o600);
+  // Create .npmrc
+  fs.writeFileSync(
+    '.npmrc',
+    `//npm.pkg.github.com/:_authToken=${authToken}\n//npm.pkg.github.com/:_header:Authorization=token ${authToken}\n@${scope}:registry=https://npm.pkg.github.com\n`,
+  );
 
-//   // Run yarn again, because the yarn process which is executing
-//   // this script won't pick up the .npmrc file we just created.
-//   // The original yarn process will continue after this second yarn process finishes,
-//   // and when it does it will report "success Already up-to-date."
-//   console.log(`.npmrc created. Starting yarn install...`);
-//   spawnSync('node', { stdio: 'inherit', env: { ...process.env, NETLIFY_NPMRC_DONE: true } });
-// }
+  // Is this necessary?
+  fs.chmodSync('.npmrc', 0o600);
+
+  // Run yarn again, because the yarn process which is executing
+  // this script won't pick up the .npmrc file we just created.
+  // The original yarn process will continue after this second yarn process finishes,
+  // and when it does it will report "success Already up-to-date."
+  console.log(`.npmrc created. Starting yarn install...`);
+  spawnSync('node', { stdio: 'inherit', env: { ...process.env, NETLIFY_NPMRC_DONE: true } });
+}
